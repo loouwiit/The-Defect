17dd7a6b67569680b9017842cd978fb1bbd88e4a
loouwiit <loouwiit@outlook.com>
2026-05-27 19:31:18 +0800

    fix: 修复app界面可滚动的bug

--- main/app/app.cpp
+++ main/app/app.cpp
@@ -4,6 +4,9 @@ App::App(Display* display) :
 	display{ display },
 	screen{ lv_obj_create(nullptr) }
 {
+	lv_obj_set_size(screen, lv_display_get_vertical_resolution(display->getLvglDisplay()), lv_display_get_horizontal_resolution(display->getLvglDisplay()));
+	lv_obj_set_scrollbar_mode(screen, LV_SCROLLBAR_MODE_OFF);
+	lv_obj_set_scroll_dir(screen, LV_DIR_NONE);
 }
 
 App::~App()
